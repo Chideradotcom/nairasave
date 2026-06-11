dd82375a7594d92091e92bcd09d9ec173b8eec5e
maracodez <amarachiruhty4@gmail.com>
2026-06-11 13:04:08 +0100

    benefit code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Reason from "./component/reason/Page";
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  return;
+  return (
+    <Routes>
+      <Route path='/' element={<Reason /> }  />
+    </Routes>
+  )
 }
 
 export default App;
