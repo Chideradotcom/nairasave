dbcd439ef9dc9f2da9c5578022ba725108cb2cd0
maracodez <amarachiruhty4@gmail.com>
2026-06-18 13:42:51 +0100

    data shown code

--- src/pages/dashboard/header/Header.jsx
+++ src/pages/dashboard/header/Header.jsx
@@ -4,7 +4,18 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h2>Dashboard Header Component</h2>
+		<section className={styles.usd}>
+			<h4>NET LIQUIDITY &#40;USD&#41;</h4>
+			<h2>$124,500.00 <sub className={styles.sub}>+2.4%</sub></h2>
+		</section>
+		<section className={styles.ngn}>
+			<h4>TOTAL EQUIVALENT &#40;NGN&#41; </h4>
+			<h2>₦199,200,000 <sub className={styles.sub}>@ 1,600.00</sub></h2>
+		</section>
+		<section className={styles.reward}>
+			<h4>TOTAL SAVINGS REWARD</h4>
+			<h2>₦18,450,230</h2>
+		</section>
     </header>
   );
 }
